9cd5a9fe8c269930a1d60ffe2d18f1e6099cdb6c
Michael Pérez <mpuhrez@gmail.com>
2017-04-17 20:05:39 -0400

    extend JWT ttl to a day

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -5,6 +5,8 @@
     Global backend package settings
 """
 
+from datetime import timedelta
+
 
 class Config(object):
     TWILIO_API_SID = None
@@ -12,6 +14,7 @@ class Config(object):
     TWILIO_API_SECRET = None
     SECRET_KEY = "3aa68944-4a88-4200-9b16-694e3319e236"
     JWT_AUTH_USERNAME_KEY = "email"
+    JWT_EXPIRATION_DELTA = timedelta(1)
 
 
 class TestingConfig(Config):
